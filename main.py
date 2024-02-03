826872ffeab3466a9e371e5278706df632736f6d
Lokesh Meesala <meesala.lokesh@gmail.com>
2024-02-03 13:47:44 -0500

    test 2 pass

--- main.py
+++ main.py
@@ -15,9 +15,8 @@ model = "gpt-3.5-turbo"
 
 def get_articles(topic):
     search = arxiv.Search(
-    #   query = "attention mechanisms",
     query = topic,
-    max_results = 2
+    max_results = 5
     )
     results = arxiv_client.results(search)
     abstracts = [(i+1, r.summary.replace("\n"," "),r.entry_id) for i,r in enumerate(results)]
@@ -25,11 +24,11 @@ def get_articles(topic):
 
 
 class ResearchSummarizer:
-    thread_id = None
-    assistant_id = None
+    thread_id = "thread_c97Apl6FpGOzbNdRRHzekDPM"
+    assistant_id = "asst_mSyDDFEKMFGPoQKSKhRbYV0b"
 
-    def __init__(self, model: str = model) -> None:
-        self.openai_client = openai.OpenAI()
+    def __init__(self, model: str = model):
+        self.openai_client = openai_client
         self.model = model
         self.assistant = None
         self.thread = None
@@ -46,112 +45,107 @@ class ResearchSummarizer:
             )
         
     def create_assistant(self, name, instructions, tools):
-            if not self.assistant:
-                assistant_obj = self.openai_client.beta.assistants.create(
-                    name=name,
-                    instructions=instructions,
-                    tools=tools,
-                    model=self.model
-                )
-                ResearchSummarizer.assistant_id = assistant_obj.id
-                self.assistant = assistant_obj
-            print(f"Assist ID -- {self.assistant_id}")
+        if not self.assistant:
+            print(f"Assistant not found creating new one.")
+            assistant_obj = self.openai_client.beta.assistants.create(
+                name=name,
+                instructions=instructions,
+                tools=tools,
+                model=self.model
+            )
+            ResearchSummarizer.assistant_id = assistant_obj.id
+            self.assistant = assistant_obj
+            print(f"Assist ID -- {self.assistant.id}")
     
     def create_thread(self):
-            if not self.thread:
-                thread_obj = self.openai_client.beta.threads.create()
-                ResearchSummarizer.thread_id = thread_obj.id
-                self.thread = thread_obj
-            print(f"ThreadID -- {self.thread_id}")
+        if not self.thread:
+            print(f"Thread not found creating new one.")
+            thread_obj = self.openai_client.beta.threads.create()
+            ResearchSummarizer.thread_id = thread_obj.id
+            self.thread = thread_obj
+            print(f"Thread ID -- {self.thread.id}")
 
     def add_message_to_thread(self, role, content):
-            if self.thread:
-                self.openai_client.beta.threads.messages.create(
-                    thread_id=self.thread.id,
-                    role=role,
-                    content=content
-                )
+        if self.thread:
+            self.openai_client.beta.threads.messages.create(
+                thread_id=self.thread.id,
+                role=role,
+                content=content
+            )
 
     def run_assistant(self, instructions):
-            if self.thread and self.assistant:
-                self.run = self.openai_client.beta.threads.runs.create(
-                    thread_id=self.thread.id,
-                    assistant_id=self.assistant.id,
-                    instructions=instructions
-                )
+        if self.thread and self.assistant:
+            self.run = self.openai_client.beta.threads.runs.create(
+                thread_id=self.thread.id,
+                assistant_id=self.assistant.id,
+                instructions=instructions
+            )
 
     def process_message(self):
-            if self.thread:
-                messages = self.openai_client.beta.threads.messages.list(
-                    thread_id=self.thread.id
-                )
-                summary = []
+        if self.thread:
+            messages = self.openai_client.beta.threads.messages.list(
+                thread_id=self.thread.id
+            )
+            summary = []
 
-                last_message = messages.data[0]
-                role = last_message.role
-                response = last_message.content[0].text.value
-                summary.append(response)
+            last_message = messages.data[0]
+            role = last_message.role
+            response = last_message.content[0].text.value
+            summary.append(response)
 
-                self.summary = "\n".join(summary)
-                print(f"Summary -- {role.capitalize()}: ==> {response}")
+            self.summary = "\n".join(summary)
+            print(f"Summary -- {role.capitalize()}: ==> {response}")
 
     def call_required_functions(self, required_actions):
-            if not self.run:
-                return
-            tool_outputs = []
-            for action in required_actions["tool_calls"]:
-                func_name = action["function"]["name"]
-                arguments = json.loads(action["function"]["arguments"])
+        if not self.run:
+            return
+        tool_outputs = []
+        for action in required_actions["tool_calls"]:
+            func_name = action["function"]["name"]
+            arguments = json.loads(action["function"]["arguments"])
 
-                if func_name == "get_articles":
-                    output = get_articles(topic=arguments["topic"])
-                    # print(f"ARTICLES ---- {output}")
-                    final_str = ""
-                    suffix_str = ""
-                    for item in output:
-                        final_str += str(item[0])+"\n"+item[1]+"\n\n"
-                        suffix_str += "["+str(item[0])+"] "+ str(item[2])+"\n\n"
-                    print(f"FINAL STR ---- {final_str}")
-                    print(f"SUFFIX STR ---- {suffix_str}")
-            
-                    tool_outputs.append({"tool_call_id": action["id"],
-                                         "output": final_str,
-                                         "suffix": suffix_str})
-                else:
-                    raise ValueError(f"Unknown function: {func_name}")   
-                                 
-            print("Submitting outputs to the Assistant")
-            self.openai_client.beta.threads.runs.submit_tool_outputs(
-                thread_id=self.thread.id,
-                run_id=self.run.id,
-                tool_outputs=tool_outputs
-            )
-            return suffix_str
+            if func_name == "get_articles":
+                output = get_articles(topic=arguments["topic"])
+                # print(f"ARTICLES ---- {output}")
+                final_str = ""
+                for item in output:
+                    final_str += "["+str(item[0])+"] "+str(item[2])+"\n"+item[1]+"\n\n"
+                print(f"FINAL STR ---- {final_str}")
+        
+                tool_outputs.append({"tool_call_id": action["id"],
+                                     "output": final_str
+                                     })
+            else:
+                raise ValueError(f"Unknown function: {func_name}")   
+                             
+        print("Submitting outputs to the Assistant")
+        self.openai_client.beta.threads.runs.submit_tool_outputs(
+            thread_id=self.thread.id,
+            run_id=self.run.id,
+            tool_outputs=tool_outputs
+        )
 
     def get_summary(self):
-            return self.summary
+        return self.summary
     
     def wait_for_completion(self):
-            if self.thread and self.run:
-                suffix_str = ""
-                while True:
-                    time.sleep(5)
-                    run_status = self.openai_client.beta.threads.runs.retrieve(
-                        thread_id=self.thread.id,
-                        run_id=self.run.id
-                    )
-                    print(f"RUN STATUS -- {run_status.model_dump_json(indent=4)}")
+        if self.thread and self.run:
+            while True:
+                time.sleep(5)
+                run_status = self.openai_client.beta.threads.runs.retrieve(
+                    thread_id=self.thread.id,
+                    run_id=self.run.id
+                )
+                print(f"RUN STATUS -- {run_status.model_dump_json(indent=4)}")
 
-                    if run_status.status == "completed":
-                        self.process_message()
-                        return suffix_str
-                        # break
-                    elif run_status.status == "requires_action":
-                        print("FUNCTION CALLING NOW")
-                        suffix = self.call_required_functions(
-                            required_actions=run_status.required_action.submit_tool_outputs.model_dump()
-                        )
-                        suffix_str = suffix
+                if run_status.status == "completed":
+                    self.process_message()
+                    break
+                elif run_status.status == "requires_action":
+                    print("FUNCTION CALLING NOW")
+                    self.call_required_functions(
+                        required_actions=run_status.required_action.submit_tool_outputs.model_dump()
+                    )
 
     def run_steps(self):
         run_steps = self.openai_client.beta.threads.runs.steps.list(
@@ -176,7 +170,7 @@ def main():
         if submit_button:
             summarizer.create_assistant(
                 name="Research Topic Summarizer",
-                instructions="You are a helpful assistant that summarizes the list of research articles given by the user. You will be given a numbered list of research articles. You need to summarize them into a single article and refer the numbers in your summary.",
+                instructions="You are a helpful assistant that creates an abstract on the topic by referring to the research articles given by the user. You will be given a numbered list of research articles and reference links. Please add references using the given links.",
                 tools=[
                     {
                         "type": "function",
@@ -202,15 +196,17 @@ def main():
 
             summarizer.add_message_to_thread(
                 role="user",
-                content=f"Here are the research articles. Please summarize them into one article and refer the numbers. \n{instructions}"
+                content=f"Create an abstract on the topic by referring to these research articles. \n{instructions}. Please add references using the given links."
             )
 
-            summarizer.run_assistant(instructions="Here are the research articles. Please summarize them into one article and refer the numbers.")
+            summarizer.run_assistant(instructions="Create an article on the topic by referring to these research articles. Please add references using the given links.")
 
-            suffix_str = summarizer.wait_for_completion()
+            summarizer.wait_for_completion()
             summary = summarizer.get_summary()
 
-            st.write(summary+"\n\n"+suffix_str)
+            st.write(summary)
+            # st.text(summary)
+            
             # st.text("Run Steps:")
             # st.code(summarizer.run_steps())
 
